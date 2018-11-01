a76b63556179fee3c36d51c8d65ae3ee396328e3
Yuki N <lunarbase@outlook.com>
2018-11-01 04:23:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  index = 1
+  array.each do |name|
+    puts "#{index}. #{name}"  
+    index += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.collect do |arr|
+    arr.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? do |arr|
+   arr.length > 4
+ end
 end
 
-def find_the_cheese# code an argument here
+
+
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.find do |type|
+     if type == "chedder"
+       type
+     else 
+       return nil  
+     end
+   end
 end
